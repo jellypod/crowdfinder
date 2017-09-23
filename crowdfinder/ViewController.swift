3957e10f14f206997f01c8a1702391309e25fdd0
Ravi <ravi.nleague@gmail.com>
2017-09-24 08:35:25 +1000

    Initial Commit

---
+++ crowdfinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crowdfinder
+//
+//  Created by Ravichandra Challa on 24/9/17.
+//  Copyright © 2017 Ravichandra Challa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
